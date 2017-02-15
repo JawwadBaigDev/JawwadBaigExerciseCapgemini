f9c9d3668c8fa713268f574af220bdce049d3526
Jawwad Baig <jawwadbaig@gmail.com>
2017-02-15 15:31:35 +0000

    Jawwad: Added ShopBilling Class and first test

---
+++ JawwadBaigExerciseCapgemini/UnitTestsForShopSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace JawwadBaigExerciseCapgemini
+{
+
+    public class ShopBillingSystem
+    {
+
+        public double StandardBill(List<string> itemList)
+        {
+            return 0;
+
+        }
+
+    }
+
+
+
+    [TestClass]
+    public class UnitTestsForShopSystem
+    {
+        // Step 1
+        [TestMethod]
+        public void StandardBillCanBeReturned()
+        {
+            ShopBillingSystem taxCodeFinder = new ShopBillingSystem();
+            List<string> itemList = new List<string>();
+            Assert.AreEqual(taxCodeFinder.StandardBill(itemList), 0);
+        }
+    }
+
+
+
+}
